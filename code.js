ca85c15de12a230787675744adc23c933c6c26f4
agustin <agussosa@hotmail.es>
2023-12-17 16:17:32 -0300

    update visual/etiquetas

--- code.js
+++ code.js
@@ -30,14 +30,57 @@ function noticias(source){
     source.forEach(element => {
         notiCard += `<article class="noti-card">
         <h2 class="noti-card-title">${element.name}</h2>
-        <span class = "noti-card-category">${element.category}</span>
-        <span class = "noti-card-category">${element.language}</span>
+        <div class="noti-card-section-category">
+            <span class="noti-card-category">${element.category}</span>
+            <span class="noti-card-category">${element.language}</span>
+        </div>
+
         <div class="noti-card-div">
             <p class="noti-card-div-p">${element.description}</p>
         </div>
+
+        <div class="noti-card-divButton">
+            <button class="noti-card-button">Ve la noticia completa</button>
+        </div>
     </article>`
-    } )
+    })
     return notiCard
 }
 
 NewsApis(endpoint, `.noticias`, noticias)
+
+
+function filter(source){
+    const uniqueEtiqueta = new Set()
+
+    source.forEach(element => {
+        uniqueEtiqueta.add(element.category)
+        console.log(uniqueEtiqueta)
+    })
+    console.log(source)
+    let etiqueta = ``
+
+    uniqueEtiqueta.forEach(element => {
+        
+        etiqueta += `<button class="noti-filter-button">${element}</button>`
+    })
+    console.log(etiqueta)
+    return etiqueta
+
+
+    
+}
+
+NewsApis(endpoint, `.noti-filter`, filter)
+
+
+
+
+/*const botonFilter = document.querySelector("noti-filter-button")
+botonFilter.addEventListener("click", function(){
+
+})
+
+function filter(){
+
+}*/
